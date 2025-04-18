bc334e551b6b2c1ddd99c5635b4f7bb3b8a41bf1
Krish Soni <sonikrish994@gmail.com>
2025-04-18 19:30:26 +0530

    Enhance server functionality by adding a new CORS origin and improving comments for clarity

--- src/server.ts
+++ src/server.ts
@@ -8,29 +8,26 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:3000", "https://codenow.vercel.app"],
+    origin: ["http://localhost:3000", "https://codenow.vercel.app","https://codenow.krishsoni.co"],
     methods: ["GET", "POST"],
   },
 });
 
 app.use(cors());
-app.use(express.json()); // To parse JSON body in requests
+app.use(express.json());
 
-// In-memory store for shared code (replace with database for production)
 const codeStore: { [key: string]: string } = {};
 
 app.get('/', (req, res) => {
   res.json({ message: 'CodeNow server is running' });
 });
 
-// Endpoint to save the code
 app.post('/api/saveCode', (req, res) => {
   const { id, code } = req.body;
-  codeStore[id] = code;  // Store code using the unique ID
+  codeStore[id] = code;
   res.status(200).json({ message: 'Code saved successfully' });
 });
 
-// Endpoint to get the code by ID
 app.get('/api/getCode/:id', (req, res) => {
   const { id } = req.params;
   const code = codeStore[id];
@@ -41,8 +38,7 @@ app.get('/api/getCode/:id', (req, res) => {
   }
 });
 
-// Socket.io for real-time code updates
-let sharedCode = ''; // Shared code in-memory
+let sharedCode = '';
 
 io.on('connection', (socket) => {
   console.log(`Client connected: ${socket.id}`);
@@ -51,11 +47,11 @@ io.on('connection', (socket) => {
 
   socket.on('codeChange', ({ newCode, url }) => {
     if (newCode !== undefined) {
-      sharedCode = newCode; // Update shared code
+      sharedCode = newCode;
       console.log(`Code change from URL: ${url || 'Unknown URL'}`);
       console.log(`New Code: ${newCode}`);
       
-      socket.broadcast.emit('codeUpdate', newCode); // Broadcast the updated code
+      socket.broadcast.emit('codeUpdate', newCode);
     } else {
       console.error('Received codeChange with undefined newCode');
     }
